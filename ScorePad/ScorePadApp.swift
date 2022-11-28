197a8372beaedd14b54ea8f1c616ae7807e202ee
Nathan Taylor <ntaylor@apple.com>
2022-11-28 09:20:58 -0800

    Initial Commit

---
+++ ScorePad/ScorePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorePadApp.swift
+//  ScorePad
+//
+//  Created by Nathan Taylor on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ScorePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
